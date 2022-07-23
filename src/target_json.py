65285f1d90015727c41591edef8da2757932f3d9
Sergey <sergei2003@bk.ru>
2022-07-23 10:08:41 +0300

    correct errors

--- src/target_json.py
+++ src/target_json.py
@@ -13,10 +13,6 @@ with open("/Users/sergejdmitriev/Otus/qa_python/auto-web-qa-2206/users.json", "r
     for user in users:
         users_list.append(Users(**user))
 
-for user in users_list:
-    if user.isActive:
-        active_user_list.append(user)
-
 books_list = []
 with open('/Users/sergejdmitriev/Otus/qa_python/auto-web-qa-2206/books.csv', newline='') as f:
     reader = csv.DictReader(f)
@@ -26,18 +22,18 @@ with open('/Users/sergejdmitriev/Otus/qa_python/auto-web-qa-2206/books.csv', new
 
 
 def final_json():
-    for i in range(len(active_user_list) - 1, -1, -1):
+    for i in range(len(users_list) - 1, -1, -1):
         book_u = []
-        user_cnt = len(active_user_list)
+        user_cnt = len(users_list)
         book_cnt = len(books_list)
         user_books_cnt = book_cnt // user_cnt
-        t = Target(active_user_list[i].name, active_user_list[i].gender, active_user_list[i].address, active_user_list[i].age, book_u)
+        t = Target(users_list[i].name, users_list[i].gender, users_list[i].address, users_list[i].age, book_u)
         for j in range(len(books_list) - 1, -1, -1):
             t.books.append(books_list[j])
             user_books_cnt -= 1
             books_list.remove(books_list[j])
             if user_books_cnt == 0:
-                active_user_list.remove(active_user_list[i])
+                users_list.remove(users_list[i])
                 break
         user_books.append(t)
     return user_books
