5eddb89603677f251aacc6523fd087fc0175256c
Sergey <sergei2003@bk.ru>
2022-07-31 04:58:50 +0300

    dataclass

--- src/Target.py
+++ src/Target.py
@@ -1,13 +1,17 @@
 from typing import List
-
+from dataclasses import dataclass
 from src.Book import Book
 
-
+@dataclass
 class Target:
-
-    def __init__(self, name, gender, address, age, books: List[Book]):
-        self.name = name
-        self.gender = gender
-        self.address = address
-        self.age = age
-        self.books = books
+    name: str
+    gender: str
+    address: str
+    age: int
+    books: List[Book]
+ #   def __init__(self, name, gender, address, age, books: List[Book]):
+ #       self.name = name
+ #       self.gender = gender
+ #       self.address = address
+ #       self.age = age
+ #       self.books = books
--- src/Users.py
+++ src/Users.py
@@ -1,26 +1,22 @@
+from dataclasses import dataclass, field
+from dataclasses import astuple, asdict
+
+
+@dataclass
 class Users:
-    def __init__(self, _id, index, guid, isActive, balance, picture, age, eyeColor,
-                 name, gender, company, email, phone, address, about, registered, latitude, longitude,
-                 tags, friends, greeting, favoriteFruit):
-        self._id = _id
-        self.index = index
-        self.guid = guid
-        self.isActive = isActive
-        self.balance = balance
-        self.picture = picture
-        self.age = age
-        self.eyeColor = eyeColor
-        self.name = name
-        self.gender = gender
-        self.company = company
-        self.email = email
-        self.phone = phone
-        self.address = address
-        self.about = about
-        self.registered = registered
-        self.latitude = latitude
-        self.longitude = longitude
-        self.tags = [tags]
-        self.friends = [friends]
-        self.greeting = greeting
-        self.favoriteFruit = favoriteFruit
+    age: int
+    name: str
+    gender: str
+    address: str
+
+
+def __eq__(self, other):
+    if other.__class__ is not self.__class__:
+        return NotImplemented
+    return (self.age,
+            self.name,
+            self.gender,
+            self.address) == (other.age,
+                              other.name,
+                              other.gender,
+                              other.address)
