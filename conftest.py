82fac2e234d82a7da569cade97de71ebfb26b66b
Sergey <sergei2003@bk.ru>
2022-10-24 09:15:43 +0300

    restore cart tests

---
+++ conftest.py
@@ -0,0 +1,103 @@
+import datetime
+import logging
+
+import pytest
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.firefox.service import Service as FFService
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--url", action="store", default="http://localhost/")
+    parser.addoption("--drivers", action="store", default=os.path.expanduser("~/Downloads/drivers"))
+    parser.addoption("--tolerance", type=int, default=5)
+    parser.addoption("--log_level", action="store", default="INFO")
+    parser.addoption("--executor", action="store", default="local")
+    parser.addoption("--bversion", action="store", default="104.0.5112")
+    parser.addoption("--vnc", action="store_true", default=False)
+    parser.addoption("--logs", action="store_true", default=False)
+    parser.addoption("--video", action="store_true", default=False)
+
+
+@pytest.fixture
+def browser(request):
+    # Сбор параметров запуска для pytest
+    browser = request.config.getoption("--browser")
+    url = request.config.getoption("--url")
+    drivers = request.config.getoption("--drivers")
+    tolerance = request.config.getoption("--tolerance")
+    log_level = request.config.getoption("--log_level")
+    executor = request.config.getoption("--executor")
+    vnc = request.config.getoption("--vnc")
+    logs = request.config.getoption("--logs")
+    video = request.config.getoption("--video")
+    version = request.config.getoption("--bversion")
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+
+    logger.info("===> Test {} started at {}".format(request.node.name, datetime.datetime.now()))
+
+    executor_url = f"http://{executor}:4444/wd/hub"
+
+    if executor != "local":
+        capabilities = {
+            "browserName": browser,
+            "browserVersion": version,
+            "selenoid:options": {
+                "enableVNC": vnc,
+                "enableVideo": video,
+                "enableLog": logs
+            },
+            "name": "name"
+        }
+        driver = webdriver.Remote(
+            desired_capabilities=capabilities,
+            command_executor=executor_url
+        )
+
+    elif browser == "chrome":
+        service = ChromiumService(executable_path=drivers + "/chromedriver104")
+        driver = webdriver.Chrome(service=service)
+    elif browser == "firefox":
+        service = FFService(executable_path=drivers + "/geckodriver")
+        driver = webdriver.Firefox(service=service)
+    elif browser == "opera":
+        driver = webdriver.Opera(executable_path=drivers + "/operadriver")
+    else:
+        driver = webdriver.Safari()
+    driver.maximize_window()
+
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
+    driver.get(url)
+    driver.url = url
+    driver.t = tolerance
+
+    logger.info("Browser:{}, Session {}".format(browser, driver.session_id, driver.capabilities))
+
+    def clear_loggers():
+        """Remove handlers from all loggers"""
+        loggers = [logging.getLogger()] + list(logging.Logger.manager.loggerDict.values())
+        for log in loggers:
+            if not hasattr(log, "handlers"):
+                continue
+            for handler in log.handlers[:]:
+                log.removeHandler(handler)
+
+    def fin():
+        driver.quit()
+        logger.info("driver.quit {}, at {}".format(driver.session_id, datetime.datetime.now()))
+        logger.info("===> Test {} finished at {}".format(request.node.name, datetime.datetime.now()))
+        clear_loggers()
+
+    request.addfinalizer(fin)
+
+    return driver
---
+++ src/page_objects/RegistrationPage.py
@@ -0,0 +1,80 @@
+from selenium.webdriver.common.by import By
+
+from src.page_objects.BasePage import BasePage
+
+
+class RegistrationPage(BasePage):
+    PATH = '/index.php?route=account/register'
+
+    FIRSTNAME_INPUT = (By.CSS_SELECTOR, "input[name='firstname']")
+    LASTNAME_INPUT = (By.CSS_SELECTOR, "input[name='lastname']")
+    EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='email']")
+    TELEPHONE_INPUT = (By.CSS_SELECTOR, "#input-telephone")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "#input-password")
+    PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#input-confirm")
+    PRIVACY_POLICY = (By.XPATH, "//input[@type='checkbox']")
+    CONTINUE_BUTTON = (By.XPATH, "//input[@type='submit']")
+
+    REGISTER_ACCOUNT_TEXT = (By.XPATH, "//div[@id='account-register']//h1")
+    YOUR_PERSONAL_DETAILS_TEXT = (By.XPATH, "//div[@id='account-register']//legend")
+    SUCCESS_TITLE = (By.XPATH, "//div[@id='content']/h1")
+
+    def open_registration_page(self):
+        self._open_page(self.PATH)
+        return self
+
+    def get_success_title(self):
+        title = self._verify_element_presence(self.SUCCESS_TITLE)
+        return title.text
+
+    def fill_telephone(self, value):
+        self._input(self.TELEPHONE_INPUT, value)
+        return self
+
+    def fill_password(self, value):
+        self._input(self.PASSWORD_INPUT, value)
+        return self
+
+    def fill_confirm(self, value):
+        self._input(self.PASSWORD_CONFIRM, value)
+        return self
+
+    def click_privacy_policy(self):
+        self._click(self.PRIVACY_POLICY)
+        return self
+
+    def click_continue(self):
+        self._click(self.CONTINUE_BUTTON)
+        return self
+
+    def fill_firstname(self, value):
+        self._input(self.FIRSTNAME_INPUT, value)
+        return self
+
+    def fill_lastname(self, value):
+        self._input(self.LASTNAME_INPUT, value)
+        return self
+
+    def fill_email(self, value):
+        self._input(self.EMAIL_INPUT, value)
+        return self
+
+    def verify_firstname_input_presence(self):
+        self._verify_element_presence(self.FIRSTNAME_INPUT)
+        return self
+
+    def verify_lastname_input_presence(self):
+        self._verify_element_presence(self.LASTNAME_INPUT)
+        return self
+
+    def verify_email_input_presence(self):
+        self._verify_element_presence(self.EMAIL_INPUT)
+        return self
+
+    def verify_register_account_text_presence(self):
+        self._verify_element_with_text_presence(self.REGISTER_ACCOUNT_TEXT, "Register Account")
+        return self
+
+    def verify_your_personal_details_text_presence(self):
+        self._verify_element_with_text_presence(self.YOUR_PERSONAL_DETAILS_TEXT, "Your Personal Details")
+        return self
